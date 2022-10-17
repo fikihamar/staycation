b7fe96dd2037772027903fea2749a6bf94dda5ae
fikihamar <fikihamar05@gmail.com>
2022-10-17 10:26:28 +0700

    slicing

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+    colors: {
+      'primary-variant': '#152C5B',
+      'primary': '#2B4CDC',
+      'secondary': '#7B7B7B'
+    },
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      serif: ['Poppins', 'serif'],
+      body: ['Poppins']
+    },
+    flex: {
+      '60': '60%',
+      '50': '50%',
+      '40': '40%'
+    }
+  },
+  plugins: [
+  ],
+}
