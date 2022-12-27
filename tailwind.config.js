a258896cd05188e3070dc722682bab31d9411104
fikihamar <fikihamar05@gmail.com>
2022-12-28 00:49:46 +0700

    [UPDATE] index and details done, booking-1 ongoing

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,28 @@ module.exports = {
       colors: {
         'primary-variant': '#152C5B',
         'primary': '#2B4CDC',
-        'secondary': '#7B7B7B'
+        'secondary': '#D9D9D9',
+        'secondary-variant': '#7B7B7B',
+        'cyan': '#1ABC9C',
+        'danger': '#EB5757',
+        'grey': '#E5E5E5',
+        'grey-2': '#BCC3D1',
+        'white': '#FFFFFF'
+
+      },
+      flex: {
+        '60': '60%',
+        '50': '50%',
+        '40': '40%'
       }
     },
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
       serif: ['Poppins', 'serif'],
       body: ['Poppins']
-    },
-    flex: {
-      '60': '60%',
-      '50': '50%',
-      '40': '40%'
     }
   },
   plugins: [
+    require('flowbite/plugin')
   ],
 }
